fffcd694a4c9348708e27e60290bfa240aa78e47
n0tybynature <74768895+n0tybynature@users.noreply.github.com>
2021-02-16 17:06:16 -0800

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {connect} from "react-redux";
+import {removeAdd} from "./../actions/featuresActions";
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+      className="button"
+      onClick={() => {
+          props.removeAdd(props.feature)
+        }
+      } >X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = (state, props) => {
+  return {
+    ...state,
+    feature: props.feature
+  }
+}
+
+
+
+export default connect(mapStateToProps,{removeAdd})(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { addMore } from '../actions/featuresActions';
+import { connect } from "react-redux";
+
 
 const AdditionalFeature = props => {
+  console.log(props)
+
+
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button"
+       onClick = {() => {
+         props.addMore(props.feature)
+       }}
+       >Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+
+
+const mapStateToProps = (state, props) => {
+  return {
+    ...state,
+    feature: props.feature
+  }
+}
+
+
+
+export default connect(mapStateToProps, {addMore})(AdditionalFeature);
